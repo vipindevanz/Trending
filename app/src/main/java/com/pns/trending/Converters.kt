4f7ff8dd936cfa8784cea8db7a44493b698002a2
Vipin dev <officialvipindev@gmail.com>
2022-12-16 09:20:35 +0530

    Add Converter class to convert list into json

---
+++ app/src/main/java/com/pns/trending/Converters.kt
@@ -0,0 +1,14 @@
+package com.pns.trending
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.pns.trending.models.BuiltBy
+
+class Converters {
+
+    @TypeConverter
+    fun listToJson(value: List<BuiltBy>?) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<BuiltBy>::class.java).toList()
+}
