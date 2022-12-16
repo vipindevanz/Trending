1d0578c4b72f5c357ac98f38812bb7ea247daf89
Vipin dev <officialvipindev@gmail.com>
2022-12-16 10:27:33 +0530

    Add repo entity

---
+++ app/src/main/java/com/pns/trending/data/entities/Repo.kt
@@ -0,0 +1,26 @@
+package com.pns.trending.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class BuiltBy {
+    var href: String? = null
+    var avatar: String? = null
+    var username: String? = null
+}
+
+@Entity
+data class Repo (
+    var author: String,
+    var name: String,
+    var avatar: String,
+    @PrimaryKey
+    var url: String = "",
+    var description: String,
+    var language: String,
+    var languageColor: String,
+    var stars : Int,
+    var forks : Int,
+    var currentPeriodStars : Int,
+    var builtBy: List<BuiltBy>
+)
